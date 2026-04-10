60320db4f327668e8f744c4b7728687e0254a90e
Ubuntu <ubuntu@ip-172-31-24-30.ec2.internal>
2026-04-10 13:12:18 +0000

    Convert Java app to Web Server on port 8080

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
+
 FROM openjdk:27-ea-oraclelinux10
 WORKDIR /app
 COPY hello.java .
 RUN javac hello.java
+# Add this line to open the port
+EXPOSE 8080
 CMD ["java", "hello"]
--- hello.java
+++ hello.java
@@ -1,5 +1,28 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello from Docker!");
+    public static void main(String[] args) throws IOException {
+        // Create a server listening on Port 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); 
+        System.out.println("Server started on port 8080...");
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "<h1>Success! Jenkins Pipeline is Live</h1>";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
